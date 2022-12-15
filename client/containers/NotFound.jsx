525ba8a29f4c2e8e0aa3e58ed881a59d20245aa0
jasonkuyper <60154156+jasonkuyper@users.noreply.github.com>
2022-12-15 14:50:42 -0500

    Finished creating basic 404 page. Further styling needed.

--- client/containers/NotFound.jsx
+++ client/containers/NotFound.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import dog from '../resources/images/404-dog.jpg';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    console.log('Go Home button clicked');
+    navigate('/');
+  };
+
   return (
     <div>
-      <h1>404 Error: Not Found</h1>
+      <h1>Page Not Found</h1>
+      <p>Uh oh, the page you were looking for was not found.</p>
       <img src={dog}></img>
+      <button onClick={goHome}>Go Home</button>
     </div>
   );
 };
