bef24e4d15bacd765be7014f1c346175058d1c4b
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2022-12-15 17:35:34 -0500

    added grid to dashboard

--- client/containers/dashboard-container.jsx
+++ client/containers/dashboard-container.jsx
@@ -9,17 +9,21 @@ const DashboardContainer = (props) => {
         <h1 >
             DashboardContainer!
         </h1>
-        <div class="container">
+        <div class="dashboard-container">
             <div class="nav-bar">Nav Bar</div>
             <div class="tool-bar">
                 <h2>Tool Bar</h2>
                 <div><button className="btn btn-accent btn-outline">Dashboard Button</button></div>
                 <div><button className="btn btn-primary">Dashboard Button 2</button></div>
-                <div><button className="btn btn-secondary">Dashboard Button 2</button></div>
-                <div><button className="btn rounded-full">Dashboard Button 2</button></div>
-
+                <div><button className="btn text-red-500">Dashboard Button 2</button></div>
+                <div><button className="btn rounded-full font-style: italic">Dashboard Button 2</button></div>
+                <div><button className="btn border-color: #60A5FA;">Dashboard Button 2</button></div>
+                <div><button className="btn">Dashboard Button 2</button></div>
             </div>
-            <div class="view-container">View Container</div>
+            <div className="view-container border-solid border-2 border-indigo-600">
+                <h2>View Container</h2>
+                <div>Views!</div>
+            </div>
         </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,24 @@ module.exports = {
     './client/containers/landing-page-container.jsx',
     './client/components/**/*.{js,jsx,ts,tsx}'
 ],
+  mode: 'jit',
+  purge: [
+    './dist/*.html',
+    './client/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  }
 }
