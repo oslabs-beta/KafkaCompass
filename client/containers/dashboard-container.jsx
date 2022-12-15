98cd5fca03774cbb416c92564a8d91c971a2070f
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2022-12-15 16:18:28 -0500

    changes on dashboard before merge

--- client/containers/dashboard-container.jsx
+++ client/containers/dashboard-container.jsx
@@ -11,7 +11,14 @@ const DashboardContainer = (props) => {
         </h1>
         <div class="container">
             <div class="nav-bar">Nav Bar</div>
-            <div class="tool-bar">Tool Bar</div>
+            <div class="tool-bar">
+                <h2>Tool Bar</h2>
+                <div><button className="btn btn-accent btn-outline">Dashboard Button</button></div>
+                <div><button className="btn btn-primary">Dashboard Button 2</button></div>
+                <div><button className="btn btn-secondary">Dashboard Button 2</button></div>
+                <div><button className="btn rounded-full">Dashboard Button 2</button></div>
+
+            </div>
             <div class="view-container">View Container</div>
         </div>
         </>
