284cb18f747881af2ed9fc593f06e5481d0f07c7
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-12-15 17:33:18 -0500

    landing page styling

--- client/containers/landing-page-container.jsx
+++ client/containers/landing-page-container.jsx
@@ -3,6 +3,7 @@ import { redirect } from "react-router";
 import { useNavigate } from 'react-router-dom';
 import '../static/styles.css'
 
+
 const LandingPage = (props) => {
     const navigate = useNavigate();
 
@@ -12,12 +13,13 @@ const LandingPage = (props) => {
     }
 
     return (
-        <div>
-            <h1>LandingPage!</h1>
-            <button className="bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg text-3xl">THIS IS A TEST BUTTON</button>
-            <button className="btn">DAISY UI BUTTON</button>
-            <button onClick={openDashboard}>Navigate to Dashboard</button>
-        </div>
+        <main className='landing-container'>
+            <article className='font-mono'>
+                <h1 className='text-3xl'>Kafka Compass</h1>
+                <p className='my-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </article>
+            <button className="btn" onClick={openDashboard}>Navigate to Dashboard</button>
+        </main>
     )
 }
 
