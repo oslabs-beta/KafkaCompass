a941d0c65d231b87cce0b7e0f550e46f515d0982
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2022-12-15 17:54:40 -0500

    changes to dashboard grid before merging dev

--- client/containers/dashboard-container.jsx
+++ client/containers/dashboard-container.jsx
@@ -11,14 +11,8 @@ const DashboardContainer = (props) => {
         </h1>
         <div class="dashboard-container">
             <div class="nav-bar">Nav Bar</div>
-            <div class="tool-bar">
+            <div class="tool-bar border-solid border-2 border-indigo-600">
                 <h2>Tool Bar</h2>
-                <div><button className="btn btn-accent btn-outline">Dashboard Button</button></div>
-                <div><button className="btn btn-primary">Dashboard Button 2</button></div>
-                <div><button className="btn text-red-500">Dashboard Button 2</button></div>
-                <div><button className="btn rounded-full font-style: italic">Dashboard Button 2</button></div>
-                <div><button className="btn border-color: #60A5FA;">Dashboard Button 2</button></div>
-                <div><button className="btn">Dashboard Button 2</button></div>
             </div>
             <div className="view-container border-solid border-2 border-indigo-600">
                 <h2>View Container</h2>
