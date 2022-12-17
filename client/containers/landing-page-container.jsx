24c6cd534a2db4ade96be38996f08d77cb6afdae
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-12-17 12:05:26 -0500

    refactored webpack

--- client/containers/landing-page-container.jsx
+++ client/containers/landing-page-container.jsx
@@ -2,10 +2,9 @@ import React, { useState, useRef } from "react";
 import { useForm } from 'react-hook-form'
 import { redirect } from "react-router";
 import { useNavigate } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
 import Navbar from '../components/nav-bar';
+import Auth from './auth';
 import '../static/styles.css'
-import Navbar from "../components/nav-bar";
 
 
 const LandingPage = (props) => {
@@ -13,14 +12,20 @@ const LandingPage = (props) => {
     const {register, handleSubmit } = useForm();
     const [username, setUsername] = useState('');
     const [displayLogin, setDisplayLogin] = useState(false);
-    const nodeRef = useRef(null);
 
     function openDashboard () {
         navigate('/dashboard');
     }
 
 
-    const onLogin = data => {
+    const onLogin = async data => {
+        try {
+            const response = await fetch('/api/login');
+            const data = await response.json();
+            console.log(data);
+        } catch(err) {
+            console.log('Login failed');
+        }
         setUsername(data.username);
     }
 
@@ -38,17 +43,7 @@ const LandingPage = (props) => {
             </article>
             <button className="btn" onClick={openDashboard}>Navigate to Dashboard</button>
 
-            {displayLogin &&
-            <>
-                <form ref={nodeRef} className='auth-form' onSubmit={handleSubmit(onLogin)} >
-                        <div className='wrapper shadow'>
-                            <input className="my-7 input input-bordered input-accent w-full max-w-xs" placeholder="Username..." {...register("username", { required: true })} name="username" type="text" />
-                            <input className="my-2 input input-bordered input-accent w-full max-w-xs" placeholder="Password..." {...register("password", { required: true })} name="password" type="password" />
-                            <input className="btn my-7 btn-outline btn-accent auth-input" value="Log In" type="submit" />
-                        </div>
-                        <button onClick={() => setDisplayLogin(false)} className="btn btn-accent">Back to main</button>
-                </form>
-            </>}
+            {displayLogin && <Auth register={register} setDisplayLogin={setDisplayLogin} onLogin={onLogin} handleSubmit={handleSubmit} /> }
         </main>
         </>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       publicPath: '/',
     },
     proxy: {
-      '/': 'http://localhost:3000',
+      '/api': 'http://localhost:3000',
       // '/': {
       //   target: 'http://localhost:8080',
       //   router: () => 'http://localhost:3000',
