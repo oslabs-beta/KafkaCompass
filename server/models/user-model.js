f902adf75b28d92d235b31d87e0a50af3b26579b
dison  ruan <disonruan@disons-mbp.myfiosgateway.com>
2022-12-18 19:09:34 -0500

    added user and cloud cluster models

---
+++ server/models/user-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  cloudCluster: [{
+    type: Schema.Types.ObjectId,
+    ref: 'CloudCluster'
+  }]
+});
+
+module.exports = new model('User', userSchema);
