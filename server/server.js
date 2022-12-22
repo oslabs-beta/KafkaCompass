a3b6df69a82278e2c4b9600db79926b46981c1b9
jasonkuyper <60154156+jasonkuyper@users.noreply.github.com>
2022-12-22 11:26:33 -0500

    New branch to work on message feature

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const session = require("express-session");
+const express = require('express');
+const session = require('express-session');
 
-const path = require("path");
+const path = require('path');
 const PORT = 3000;
 
-const mongodb = require("mongoose");
-const MONGO_URI = require("./credentials");
+const mongodb = require('mongoose');
+const MONGO_URI =
+  'mongodb+srv://jasonkuyper:12345@kafka-compass.1r5raix.mongodb.net/?retryWrites=true&w=majority'; //require("./credentials");
 
-const cloudAuthController = require("./controllers/cloud-auth-controller");
-const userController = require("./controllers/user-controller");
-const metricController = require("./controllers/metric-controller");
+const cloudAuthController = require('./controllers/cloud-auth-controller');
+const userController = require('./controllers/user-controller');
+const metricController = require('./controllers/metric-controller');
 
 const app = express();
 app.use(express.json());
@@ -19,11 +20,11 @@ mongodb.connect(MONGO_URI);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, "../dist")));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.use(
   session({
-    secret: "test",
+    secret: 'test',
     saveUninitialized: true,
     cookie: { maxAge: 50000 },
     resave: false,
@@ -31,7 +32,7 @@ app.use(
 );
 
 app.use(
-  "/api/login",
+  '/api/login',
   userController.verifyUser,
   userController.authorizeUser,
   (req, res, next) => {
@@ -40,18 +41,18 @@ app.use(
   }
 );
 
-app.get("/api/authenticate", userController.authorizeUser, (req, res, next) => {
+app.get('/api/authenticate', userController.authorizeUser, (req, res, next) => {
   res.status(200).json(req.session);
 });
 
-app.get("/api/logout", userController.logOut, (req, res, next) => {
-  console.log("user logged out");
+app.get('/api/logout', userController.logOut, (req, res, next) => {
+  console.log('user logged out');
   res.status(200).json();
 });
 
 // testing endpoint for sign up
 app.use(
-  "/api/signup",
+  '/api/signup',
   userController.createUser,
   userController.authorizeUser,
   (req, res, next) => {
@@ -65,7 +66,7 @@ app.use(
 // });
 //requests to server go here
 app.post(
-  "/api/cloud-auth",
+  '/api/cloud-auth',
   cloudAuthController.encryptCredentials,
   userController.addCloudCluster,
   (req, res) => {
@@ -74,7 +75,7 @@ app.post(
 );
 
 app.get(
-  "/api/metric",
+  '/api/metric',
   metricController.decryptKeys,
   metricController.fetchData,
   userController.addMetrics,
@@ -91,12 +92,12 @@ app.get(
 // global error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "Unknown error occurred" },
+    message: { err: 'Unknown error occurred' },
   };
   const errorObj = Object.assign(defaultErr, err);
-  console.log("Global error handler caught: ", errorObj.log);
+  console.log('Global error handler caught: ', errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
