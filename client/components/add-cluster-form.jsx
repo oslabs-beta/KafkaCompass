4d2ef5e1d584be8a22d2ee675225d28f3cfd8be1
jasonkuyper <60154156+jasonkuyper@users.noreply.github.com>
2023-01-05 16:24:40 -0500

    started adding cluster name to new cluster form

--- client/components/add-cluster-form.jsx
+++ client/components/add-cluster-form.jsx
@@ -1,106 +1,159 @@
 import React, { useState } from 'react';
 
 const AddClusterForm = () => {
-    
-     //keeps track of user inputs in 'Add New Cluster' form
-     const [newAPIKeyInput, setNewAPIKeyInput] = useState('');
-     const [newAPISecretInput, setNewAPISecretInput] = useState('');
-     const [newCloudKeyInput, setNewCloudKeyInput] = useState('');
-     const [newCloudSecretInput, setNewCloudSecretInput] = useState('');
-     const [newRESTEndpointInput, setNewRESTEndpointInput] = useState('');
-     const [newClusterIdInput, setNewClusterIdInput] = useState('');
-     const [newBootsrapServerInput, setNewBootstrapServerInput] = useState('');
+  //keeps track of user inputs in 'Add New Cluster' form
+  const [newAPIKeyInput, setNewAPIKeyInput] = useState('');
+  const [newAPISecretInput, setNewAPISecretInput] = useState('');
+  const [newCloudKeyInput, setNewCloudKeyInput] = useState('');
+  const [newCloudSecretInput, setNewCloudSecretInput] = useState('');
+  const [newRESTEndpointInput, setNewRESTEndpointInput] = useState('');
+  const [newClusterIdInput, setNewClusterIdInput] = useState('');
+  const [newBootstrapServerInput, setNewBootstrapServerInput] = useState('');
+  const [newAlias, setNewAlias] = useState('');
 
-    // submit new cluster
-    async function submitNewCluster () {
-        // create object to send to db
-        try {
-            const newCluster = {
-                API_KEY: newAPIKeyInput,
-                API_SECRET: newAPISecretInput,
-                CLOUD_KEY: newCloudKeyInput,
-                CLOUD_SECRET: newCloudSecretInput,
-                clusterId: newClusterIdInput,
-                RESTendpoint: newRESTEndpointInput,
-                bootstrapServer: newBootsrapServerInput,
-            }
-            console.log('newCluster: ', newCluster);
-            // send post request to backend
-            const data = await fetch ('/api/cloud-auth', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    },
-                body: JSON.stringify(newCluster)
-            })
-            const response = await response.json();
-            if (response.ok) {
-                console.log('cluster added')
-            }
-            else {
-                console.log('error adding cluster')
-            }
-        }
-        catch (err) {
-            console.log('network error')
-        }
-
-        //add functionality here to tell user if cluster was successfully added to DB, using status code as indicator
-        //maybe a green banner saying request was successful and red if not successful
-        //fields will clear out if this cluster was successfully added
-        setNewAPIKeyInput('');
-        setNewAPISecretInput('');
-        setNewCloudKeyInput('');
-        setNewCloudSecretInput('');
-        setNewRESTEndpointInput('');
-        setNewClusterIdInput('');
-        setNewBootstrapServerInput('');
+  // submit new cluster
+  async function submitNewCluster() {
+    // create object to send to db
+    try {
+      const newCluster = {
+        API_KEY: newAPIKeyInput,
+        API_SECRET: newAPISecretInput,
+        CLOUD_KEY: newCloudKeyInput,
+        CLOUD_SECRET: newCloudSecretInput,
+        clusterId: newClusterIdInput,
+        RESTendpoint: newRESTEndpointInput,
+        bootstrapServer: newBootsrapServerInput,
+      };
+      console.log('newCluster: ', newCluster);
+      // send post request to backend
+      const data = await fetch('/api/cloud-auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCluster),
+      });
+      const response = await response.json();
+      if (response.ok) {
+        console.log('cluster added');
+      } else {
+        console.log('error adding cluster');
+      }
+    } catch (err) {
+      console.log('network error');
     }
 
-    return (
-        <>
-        <input type="checkbox" id="my-modal-4" class="modal-toggle" />
-        <label for="my-modal-4" class="modal cursor-pointer">
-            <label class="modal-box relative" for="">
-                <h3 class="text-lg font-bold">Input Cluster Details:</h3>
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">API Key:</span>
-                    </label>
-                    <input type="text" placeholder="API Key" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewAPIKeyInput(e.target.value)} value={newAPIKeyInput}/>
-                    <label class="label">
-                        <span class="label-text">API Secret:</span>
-                    </label>
-                    <input type="text" placeholder="API Secret" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewAPISecretInput(e.target.value)} value={newAPISecretInput}/>
-                    <label class="label">
-                        <span class="label-text">Cloud Key</span>
-                    </label>
-                    <input type="text" placeholder="Cloud Key" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewCloudKeyInput(e.target.value)} value={newCloudKeyInput}/>
-                    <label class="label">
-                        <span class="label-text">Cloud Secret</span>
-                    </label>
-                    <input type="text" placeholder="Cloud Secret" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewCloudSecretInput(e.target.value)} value={newCloudSecretInput}/>
-                    <label class="label">
-                        <span class="label-text">REST Endpoint</span>
-                    </label>
-                    <input type="text" placeholder="REST Endpoint" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewRESTEndpointInput(e.target.value)} value={newRESTEndpointInput}/>
-                    <label class="label">
-                        <span class="label-text">Cluster ID:</span>
-                    </label>
-                    <input type="text" placeholder="Cluster ID" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewClusterIdInput(e.target.value)} value={newClusterIdInput}/>
-                    <div className = 'pt-4'>
-                    <label class="label">
-                        <span class="label-text">Bootstrap Server:</span>
-                    </label>
-                    <input type="text" placeholder="Bootstrap Server" class="input input-bordered w-full max-w-xs" onChange={(e) => setNewBootstrapServerInput(e.target.value)} value={newBootsrapServerInput}/>
-                    <div className = 'pt-4'></div>
-                        <button className = 'btn btn-primary' onClick={submitNewCluster}>Submit</button>
-                    </div>
-                </div>
+    //add functionality here to tell user if cluster was successfully added to DB, using status code as indicator
+    //maybe a green banner saying request was successful and red if not successful
+    //fields will clear out if this cluster was successfully added
+    setNewAPIKeyInput('');
+    setNewAPISecretInput('');
+    setNewCloudKeyInput('');
+    setNewCloudSecretInput('');
+    setNewRESTEndpointInput('');
+    setNewClusterIdInput('');
+    setNewBootstrapServerInput('');
+    setNewAlias('');
+  }
+
+  return (
+    <>
+      <input type='checkbox' id='my-modal-4' class='modal-toggle' />
+      <label for='my-modal-4' class='modal cursor-pointer'>
+        <label class='modal-box relative' for=''>
+          <h3 class='text-lg font-bold'>Input Cluster Details:</h3>
+          <div class='form-control w-full max-w-xs'>
+            <label class='label'>
+              <span class='label-text'>Cluster Name:</span>
             </label>
+            <input
+              type='text'
+              placeholder='Cluster Name'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewAlias(e.target.value)}
+              value={newAlias}
+            />
+            <label class='label'>
+              <span class='label-text'>API Key:</span>
+            </label>
+            <input
+              type='text'
+              placeholder='API Key'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewAPIKeyInput(e.target.value)}
+              value={newAPIKeyInput}
+            />
+            <label class='label'>
+              <span class='label-text'>API Secret:</span>
+            </label>
+            <input
+              type='text'
+              placeholder='API Secret'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewAPISecretInput(e.target.value)}
+              value={newAPISecretInput}
+            />
+            <label class='label'>
+              <span class='label-text'>Cloud Key</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Cloud Key'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewCloudKeyInput(e.target.value)}
+              value={newCloudKeyInput}
+            />
+            <label class='label'>
+              <span class='label-text'>Cloud Secret</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Cloud Secret'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewCloudSecretInput(e.target.value)}
+              value={newCloudSecretInput}
+            />
+            <label class='label'>
+              <span class='label-text'>REST Endpoint</span>
+            </label>
+            <input
+              type='text'
+              placeholder='REST Endpoint'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewRESTEndpointInput(e.target.value)}
+              value={newRESTEndpointInput}
+            />
+            <label class='label'>
+              <span class='label-text'>Cluster ID:</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Cluster ID'
+              class='input input-bordered w-full max-w-xs'
+              onChange={(e) => setNewClusterIdInput(e.target.value)}
+              value={newClusterIdInput}
+            />
+            <div className='pt-4'>
+              <label class='label'>
+                <span class='label-text'>Bootstrap Server:</span>
+              </label>
+              <input
+                type='text'
+                placeholder='Bootstrap Server'
+                class='input input-bordered w-full max-w-xs'
+                onChange={(e) => setNewBootstrapServerInput(e.target.value)}
+                value={newBootstrapServerInput}
+              />
+              <div className='pt-4'></div>
+              <button className='btn btn-primary' onClick={submitNewCluster}>
+                Submit
+              </button>
+            </div>
+          </div>
         </label>
-        </>
-    )
-}
+      </label>
+    </>
+  );
+};
 
 export default AddClusterForm;
--- client/containers/dashboard-container.jsx
+++ client/containers/dashboard-container.jsx
@@ -25,52 +25,48 @@ const DashboardContainer = (props) => {
 
   useEffect(() => {
     const retainedBytes = data.retained_bytes.metrics.map(
-        (topic) => topic.value
-      );
-      const topics = data.retained_bytes.metrics.map((topic) => topic.topic);
-      const valuesReq = data.request_bytes.metrics.map(
-        (topic) => topic.value
-      );
-      const valuesRes = data.response_bytes.metrics.map(
-        (topic) => topic.value
-      );
+      (topic) => topic.value
+    );
+    const topics = data.retained_bytes.metrics.map((topic) => topic.topic);
+    const valuesReq = data.request_bytes.metrics.map((topic) => topic.value);
+    const valuesRes = data.response_bytes.metrics.map((topic) => topic.value);
 
-      setTotal({
-        totalRetainedBytes: data.retained_bytes.totalValue,
-        totalReq: data.request_bytes.totalValue,
-        totalRes: data.response_bytes.totalValue,
-      });
+    setTotal({
+      totalRetainedBytes: data.retained_bytes.totalValue,
+      totalReq: data.request_bytes.totalValue,
+      totalRes: data.response_bytes.totalValue,
+    });
 
-      setChart({
-        topics: {
-          labels: topics,
-          datasets: [
-            {
-              label: "bytes",
-              data: retainedBytes,
-              backgroundColor: "rgba(64, 180, 179, 0.5)",
-              borderWidth: 1,
-            },
-          ],
-        },
-        reqRes: {
-          labels: data.request_bytes.metrics.map((topic) => topic.type),
-          datasets: [
-            {
-              label: "request bytes",
-              data: valuesReq,
-              backgroundColor: "rgba(64, 180, 179, 0.5)",
-              borderWidth: 1,
-            },
-            {
-              label: "response bytes",
-              data: valuesRes,
-              backgroundColor: "rgba(250, 73, 112, 0.5)",
-              borderWidth: 1,
-            },
-          ],
-        },
-      });
+    setChart({
+      topics: {
+        labels: topics,
+        datasets: [
+          {
+            label: 'bytes',
+            data: retainedBytes,
+            backgroundColor: 'rgba(64, 180, 179, 0.5)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      reqRes: {
+        labels: data.request_bytes.metrics.map((topic) => topic.type),
+        datasets: [
+          {
+            label: 'request bytes',
+            data: valuesReq,
+            backgroundColor: 'rgba(64, 180, 179, 0.5)',
+            borderWidth: 1,
+          },
+          {
+            label: 'response bytes',
+            data: valuesRes,
+            backgroundColor: 'rgba(250, 73, 112, 0.5)',
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
   }, []);
 
   useEffect(() => {
@@ -78,24 +74,24 @@ const DashboardContainer = (props) => {
   }, []);
 
   // dictates the view mode on dashbaord
-  const [mode, setMode] = useState("viewCluster");
+  const [mode, setMode] = useState('viewCluster');
 
   // mode switching functions
   function changeModeViewCluster() {
-    setMode("viewCluster");
+    setMode('viewCluster');
   }
   function changeModeRealtimeMonitoring() {
-    setMode("realtimeMonitoring");
+    setMode('realtimeMonitoring');
   }
   function changeModeClusterComparison() {
-    setMode("clusterComparison");
+    setMode('clusterComparison');
   }
 
   // sets current dashboard view
   let dashboardView = <></>;
-  if (mode === "viewCluster") {
+  if (mode === 'viewCluster') {
     dashboardView = (
-      <main className="cluster-container">
+      <main className='cluster-container'>
         {metricSelection.retainedBytes && (
           <>
             <Chart
@@ -124,8 +120,12 @@ const DashboardContainer = (props) => {
         )}
       </main>
     );
-  } else if (mode === "realTimeMonitoring") {
-    dashboardView = <></>;
+  } else if (mode === 'realTimeMonitoring') {
+    dashboardView = (
+      <>
+        <Messages />
+      </>
+    );
   } else {
     dashboardView = <></>;
   }
@@ -143,20 +143,20 @@ const DashboardContainer = (props) => {
 
   return (
     <>
-      <div class="drawer">
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content border-solid border-2 border-black-500">
-          <div className="mt-4 flex justify-around">
-            <div class="btn-group">
+      <div class='drawer'>
+        <input id='my-drawer' type='checkbox' class='drawer-toggle' />
+        <div class='drawer-content border-solid border-2 border-black-500'>
+          <div className='mt-4 flex justify-around'>
+            <div class='btn-group'>
               <button
-                className={mode === "viewCluster" ? "btn btn-accent" : "btn"}
+                className={mode === 'viewCluster' ? 'btn btn-accent' : 'btn'}
                 onClick={changeModeViewCluster}
               >
                 View Cluster
               </button>
               <button
                 className={
-                  mode === "realtimeMonitoring" ? "btn btn-accent" : "btn"
+                  mode === 'realtimeMonitoring' ? 'btn btn-accent' : 'btn'
                 }
                 onClick={changeModeRealtimeMonitoring}
               >
@@ -164,7 +164,7 @@ const DashboardContainer = (props) => {
               </button>
               <button
                 className={
-                  mode === "clusterComparison" ? "btn btn-accent" : "btn"
+                  mode === 'clusterComparison' ? 'btn btn-accent' : 'btn'
                 }
                 onClick={changeModeClusterComparison}
               >
@@ -172,22 +172,22 @@ const DashboardContainer = (props) => {
               </button>
             </div>
           </div>
-          <div className="flex justify-around pt-10">{dashboardView}</div>
+          <div className='flex justify-around pt-10'>{dashboardView}</div>
           {/* <!-- Page content here --> */}
         </div>
         <AddClusterForm />
-        <div class="drawer-side">
-          <label for="my-drawer" class="drawer-overlay"></label>
-          <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+        <div class='drawer-side'>
+          <label for='my-drawer' class='drawer-overlay'></label>
+          <ul class='menu p-4 w-80 bg-base-100 text-base-content'>
             <li
-              onClick={() => updateSideDrawer("retainedBytes")}
-              class={metricSelection.retainedBytes ? "bg-secondary" : ""}
+              onClick={() => updateSideDrawer('retainedBytes')}
+              class={metricSelection.retainedBytes ? 'bg-secondary' : ''}
             >
               <a>Retained bytes</a>
             </li>
             <li
-              onClick={() => updateSideDrawer("reqResBytes")}
-              class={metricSelection.reqResBytes ? "bg-secondary" : ""}
+              onClick={() => updateSideDrawer('reqResBytes')}
+              class={metricSelection.reqResBytes ? 'bg-secondary' : ''}
             >
               <a>Request/Response bytes</a>
             </li>
