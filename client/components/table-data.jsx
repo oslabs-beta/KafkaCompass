0d4faa0819dd4692d599c20461bbcfeb2d9b67eb
Ryan <ryanzarou@gmail.com>
2023-01-07 16:03:13 -0500

    added jwt authentication

--- client/components/table-data.jsx
+++ client/components/table-data.jsx
@@ -1,48 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const TableData = (props) => {
-
-    let tableRows = [];
-    let oneRow = <></>
-    for (let i = 0; i < props.tableData.name.length; i++) {
-      oneRow = (
-        <tr>
+  let tableRows = [];
+  let oneRow = <></>;
+  for (let i = 0; i < props.tableData.name.length; i++) {
+    oneRow = (
+      <tr>
         <th>{i}</th>
         <td>{props.tableData.name[i]}</td>
-        <td><div class="scrollable">
-        {props.tableData.description[i]}
-        </div></td>
+        <td>
+          <div class="scrollable">{props.tableData.description[i]}</div>
+        </td>
         <td>{props.tableData.values[i]}</td>
       </tr>
-      )
-      tableRows.push(oneRow);
-    }
+    );
+    tableRows.push(oneRow);
+  }
 
-  
-
-  useEffect(() => {
-  },[])
+  useEffect(() => {}, []);
 
   return (
     <div className="table-stats">
-    <div className="overflow-x-auto">
-  <table className="table w-full">
-    {/* <!-- head --> */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Metrics</th>
-      </tr>
-    </thead>
-    <tbody>
-      {tableRows}
-    </tbody>
-  </table>
-</div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Metrics</th>
+            </tr>
+          </thead>
+          <tbody>{tableRows}</tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TableData;
