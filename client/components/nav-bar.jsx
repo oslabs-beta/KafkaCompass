bb998bcd9f08a11f198685301916ea3abdf3dcdf
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-01-10 15:19:30 -0500

    added in daisy ui component and refactoring buttons

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -75,12 +75,74 @@ const Navbar = ({ navigate, logUserOut }) => {
   }
 
   return (
-    <div className="navbar">
-      <div className="flex-1 flex-wrap grow-[2]">{drawerButtons}</div>
-      <div className="flex-1 justify-end overflow-auto flex-wrap">
-        {logButtons}
+    <>
+      <div className="navbar">
+        <div className="flex-1 flex-wrap grow-[2]">{drawerButtons}</div>
+        <div className="flex-1 justify-end overflow-auto flex-wrap">
+          {logButtons}
+        </div>
       </div>
-    </div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost normal-case text-xl">
+              Cluster
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Add</a>
+              </li>
+              <li>
+                <a>Switch</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost normal-case text-xl">KafkaCompass</a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
