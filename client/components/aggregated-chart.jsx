ce4719ffa81c2fd02f74164f9387a0b1f43f2690
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-02-18 13:18:29 -0500

    added all aggCharts

--- client/components/aggregated-chart.jsx
+++ client/components/aggregated-chart.jsx
@@ -4,34 +4,7 @@ import "chart.js/auto";
 import { Bar, Line } from "react-chartjs-2";
 
 const AggregatedChart = (props) => {
-  // selecting metric specific to this chart
-  const metric = props.metricSelection;
-  const chartData = props.chartData[metric];
-
-  const data = chartData.info;
-
-  const { user } = useContext(NavbarContext).userState;
-  console.log("chartData: ", user.metric);
-  const connCountArr = [];
-  const labels = [];
-  for (let i = 0; i < user.metric.length; i++) {
-    labels.push(i);
-    connCountArr.push({
-      x: i,
-      y: user.metric[i].active_connection_count.metrics[0].value
-    });
-  }
-  console.log("connCountArr: ", connCountArr);
-  console.log("data: ", data);
-  const data2 = {
-    labels: labels,
-    datasets: [
-      {
-        data: connCountArr
-      }
-    ]
-  };
-
+  //metric selection and context state
   const { setMetric } = useContext(NavbarContext).metricState;
   const { sideBarMode } = useContext(NavbarContext).sideBarState;
 
@@ -42,46 +15,76 @@ const AggregatedChart = (props) => {
     setMetric(metric);
   }
 
-  return (
-    <div className="topic-chart font-mono chart-container pb-10">
-      <p style={{ fontSize: "18px" }}>{chartData.colDescription}</p>
+  // selecting metric specific to this chart
+  const metric = props.metricSelection;
+  const chartData = props.chartData[metric];
 
-      <br />
-      <Line
-        type={"line"}
-        options={{
-          maintainAspectRatio: false
-        }}
-        data={data2}
-      />
-      <Bar
-        width={"20%"}
-        type={"bar"}
-        data={data}
-        options={{
-          maintainAspectRatio: false
-        }}
-      />
-      <br />
-      {chartData.compositeChart && (
-        <>
-          <p className="chart-total">
-            {chartData.totalDescription1} <span>{chartData.totalValue1}</span>
-          </p>
-          <p className="chart-total">
-            {chartData.totalDescription2} <span>{chartData.totalValue2}</span>
-          </p>
-        </>
-      )}
+  const data = chartData.info;
 
-      {!chartData.compositeChart && (
-        <p className="chart-total">
-          {chartData.totalDescription}
-          <span>{chartData.totalValue}</span>
-        </p>
-      )}
-    </div>
-  );
+  const { user } = useContext(NavbarContext).userState;
+  console.log("chartData: ", user.metric);
+  //object with all statistics for user clusters
+  const rawMetric = user.metric;
+
+  //stats for aggregated charts
+  const aggStats = {
+    active_connection_count: "",
+    partition_count: "",
+    successful_authentication_count: "",
+    cluster_load_percent: ""
+  };
+
+  //dataset creation for charts
+  for (const stat in aggStats) {
+    const data = { labels: [], datasets: [] };
+    let statArr = [];
+    const labels = [];
+    let curr = rawMetric[0].clusterId;
+    for (let i = 0; i < rawMetric.length; i++) {
+      data.labels.push(rawMetric[i].created_at);
+      statArr.push({
+        x: i,
+        //edge case where some of the arrays were empty
+        y: rawMetric[i][stat].metrics.length
+          ? rawMetric[i][stat].metrics[0].value
+          : 0
+      });
+      if (i + 1 === rawMetric.length || rawMetric[i + 1].clusterId !== curr) {
+        const metricObj = { label: curr, data: statArr };
+        data.labels.sort();
+        data.datasets.push(metricObj);
+        if (i + 1 !== rawMetric.length) {
+          statArr = [];
+          curr = rawMetric[i + 1].clusterId;
+        }
+      }
+    }
+    aggStats[stat] = data;
+  }
+
+  console.log("data: ", aggStats);
+
+  //creating aggregated charts
+  const charts = [];
+  for (const stat in aggStats) {
+    const chart = (
+      <div className="topic-chart font-mono chart-container pb-10">
+        <p style={{ fontSize: "18px" }}>{stat}</p>
+
+        <br />
+        <Line
+          type={"line"}
+          options={{
+            maintainAspectRatio: false
+          }}
+          data={aggStats[stat]}
+        />
+      </div>
+    );
+    charts.push(chart);
+  }
+
+  return <>{charts}</>;
 };
 
 export default AggregatedChart;
