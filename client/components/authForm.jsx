d728af50ef78b6838b965f7bc05fa86eb57495a2
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-03-20 23:29:12 -0400

    added more specific headers on auth forms

--- client/components/authForm.jsx
+++ client/components/authForm.jsx
@@ -69,9 +69,16 @@ const AuthForm = ({ handleSubmit, register, onSubmit, type, navigate }) => {
             src={logoWithoutText}
             alt="KafkaCompass Logo (login)"
           />
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
+          {login && (
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          )}
+          {!login && (
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Create new account
+            </h2>
+          )}
           {/* <p className="mt-2 text-center text-sm text-gray-600">
               Or{" "}
               <a
